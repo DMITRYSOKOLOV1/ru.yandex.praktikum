390dfa16f476ffb650f0341afcf4f6f4d61b430c
Sokolov Dmitrii <sokolov.d@alcor.co.ru>
2024-06-21 11:33:49 +0300

    add showMenu

--- src/ru/restaurant/Restaurant.java
+++ src/ru/restaurant/Restaurant.java
@@ -3,4 +3,9 @@ package ru.restaurant;
 public class Restaurant {
     int pearsCount;
     String[] menu = {"Шарлотка грушевая", "Компот грушевый", "Салат из груш и лисичек", "Рулет с грушами"};
+    public void showMenu(){
+        for(int i = 0; i < menu.length; i++){
+            System.out.println("Сегодня у нас в меню " + menu[i]);
+        }
+    }
 }

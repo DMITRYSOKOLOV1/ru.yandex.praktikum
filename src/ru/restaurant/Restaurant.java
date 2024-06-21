7c8fdfa0e797afd1802adc56dc17de6dd12ae60e
Sokolov Dmitrii <sokolov.d@alcor.co.ru>
2024-06-21 11:24:39 +0300

    first commit

---
+++ src/ru/restaurant/Restaurant.java
@@ -0,0 +1,6 @@
+package ru.restaurant;
+
+public class Restaurant {
+    int pearsCount;
+    String[] menu = {"Шарлотка грушевая", "Компот грушевый", "Салат из груш и лисичек", "Рулет с грушами"};
+}
